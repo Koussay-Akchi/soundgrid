a883a7bd859f8e60d18dc44a7cf014cd5d5e6e26
Koussay Akchi <koussay.akchi@wecraft.tn>
2024-10-10 09:57:08 +0100

    feat: selectable instruments

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -3,11 +3,11 @@ import { FC, memo } from "react";
 const Footer: FC = memo(() => (
   <div className="relative bottom-0 left-0 right-0 bg-neutral-900 px-1 pb-3 pt-3 sm:px-8 sm:pb-4 sm:pt-4">
     <div className="flex flex-col items-center gap-y-2">
-    <div className=" text-neutral-500">
+      <div className=" text-neutral-500">
         Made from scratch with React and basic JavaScript
       </div>
       <div className=" text-neutral-500">
-        Sounds made with Tone.js
+        Sounds made with SoundFont player and icons from react-icons
       </div>
       <a href="https://github.com/Koussay-Akchi/soundgrid">
         <span className="text-sm text-neutral-400">Github repo</span>
--- src/SoundGrid.tsx
+++ src/SoundGrid.tsx
@@ -8,25 +8,29 @@ import {
 } from "react";
 import "./index.css";
 import Grid from "./Grid";
-import { GiXylophone } from "react-icons/gi";
-import { GiPianoKeys } from "react-icons/gi";
+import {
+  GiXylophone,
+  GiPianoKeys,
+  GiBanjo,
+  GiMusicalKeyboard,
+} from "react-icons/gi";
 import Soundfont from "soundfont-player";
 import { FaGuitar } from "react-icons/fa";
-import { GiBanjo } from "react-icons/gi";
-import { GiMusicalKeyboard } from "react-icons/gi";
 import { TbTriangleOff } from "react-icons/tb";
+import { MdKeyboardArrowDown } from "react-icons/md";
+
 const ROWS = 16;
 const notes = ["C4", "D4", "E4", "F4", "G4", "A4", "B4", "C5", "D5", "E5"];
 // gap needs to be counted here :'(
 const BOX_HEIGHT = 2.25;
 
 const instruments = [
-  "acoustic_grand_piano",
-  "acoustic_guitar_nylon",
-  "kalimba",
-  "glockenspiel",
-  "lead_1_square",
-  "banjo",
+  { name: "acoustic_grand_piano", icon: GiPianoKeys },
+  { name: "acoustic_guitar_nylon", icon: FaGuitar },
+  { name: "kalimba", icon: GiXylophone },
+  { name: "tinkle_bell", icon: TbTriangleOff },
+  { name: "lead_1_square", icon: GiMusicalKeyboard },
+  { name: "banjo", icon: GiBanjo },
 ];
 
 const SoundGrid = forwardRef((_, ref) => {
@@ -34,7 +38,9 @@ const SoundGrid = forwardRef((_, ref) => {
   const [enabledBoxes, setEnabledBoxes] = useState<boolean[][]>(
     Array.from({ length: ROWS }, () => Array(10).fill(false))
   );
-  const [selectedInstrument, setSelectedInstrument] = useState(instruments[0]);
+  const [selectedInstrument, setSelectedInstrument] = useState(
+    instruments[0].name
+  );
 
   const instrumentRef = useRef<Soundfont.Player | null>(null);
   const audioContextRef = useRef<AudioContext | null>(null);
@@ -57,10 +63,6 @@ const SoundGrid = forwardRef((_, ref) => {
       );
     }
   };
-
-  useEffect(() => {
-    loadInstrument(selectedInstrument);
-  }, [selectedInstrument]);
 
   useImperativeHandle(ref, () => ({
     resetAllBoxes: () => {
@@ -107,27 +109,33 @@ const SoundGrid = forwardRef((_, ref) => {
       <div className="text-white mb-2">
         {linePosition}/{ROWS}
       </div>
-      <select
-        value={selectedInstrument}
-        onChange={(e) => setSelectedInstrument(e.target.value)}
-        className="mb-2 p-1 rounded"
-      >
+      <div className="flex gap-2 mb-2 justify-center mt-1">
         {instruments.map((instrument) => (
-          <option key={instrument} value={instrument}>
-            {instrument.replace(/_/g, " ")}
-          </option>
+          <div key={instrument.name} className="relative">
+            {selectedInstrument === instrument.name && (
+              <MdKeyboardArrowDown
+                className="absolute -top-4 left-1/2 transform -translate-x-1/2 text-white"
+                size={20}
+              />
+            )}
+            <instrument.icon
+              size={30}
+              className={`text-white cursor-pointer ${
+                selectedInstrument === instrument.name
+                  ? "text-blue-500"
+                  : "hover:text-blue-300"
+              }`}
+              onClick={() => setSelectedInstrument(instrument.name)}
+            />
+          </div>
         ))}
-      </select>
-      <div className=" flex gap-2 mb-2 justify-center">
-        <FaGuitar size={30} />
-        <GiXylophone size={30} />
-        <GiPianoKeys size={30} />
-        <GiBanjo size={30} />
-        <TbTriangleOff size={30} />
-        <GiMusicalKeyboard size={30} />
       </div>
       <div className="flex items-center justify-center relative">
-        <Grid enabledBoxes={enabledBoxes} setEnabledBoxes={setEnabledBoxes} linePosition={linePosition} />
+        <Grid
+          enabledBoxes={enabledBoxes}
+          setEnabledBoxes={setEnabledBoxes}
+          linePosition={linePosition}
+        />
         <div
           className="absolute top-0 left-0 w-full h-full pointer-events-none z-20"
           style={{
